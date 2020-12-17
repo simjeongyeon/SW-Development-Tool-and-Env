fc7b0623dcbfbde587ed002c2d05cd4ceec8a34f
simjeongyeon <72926240+simjeongyeon@users.noreply.github.com>
2020-12-18 01:46:56 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,8 +1,12 @@
 public class Calc{
+	public int r=0;
 	public int add(int x, int y){	//2차커밋을 위한 내용변경
 		return x+y;
 	}
 	public int sub(int x, int y){
 		return x-y;
+	}
+	public void dec(int d){	//
+		r -= d;
 	}
 }
