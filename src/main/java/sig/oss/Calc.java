288002d4472513eb9362bdc1a5cae9b92b1f9b90
jeongyeon <wjddus0919@naver.com>
2020-12-18 01:01:51 +0900

    add a subtraction method and its test code in Java files

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,5 +1,8 @@
 public class Calc{
-	public int add(int x, int y){//2차커밋을 위한 내용변경
+	public int add(int x, int y){	//2차커밋을 위한 내용변경
 		return x+y;
+	}
+	public int sub(int x, int y){
+		return x-y;
 	}
 }
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -7,4 +7,9 @@ public class CalcTest {
 		Calc c = new Calc();
 		assertEquals(30, c.add(10, 20));
 	}
+	@Test
+	public void testSub() {
+		Calc c = new Calc();
+		assertEquals(-10, c.sub(10,20));
+	}
 }
